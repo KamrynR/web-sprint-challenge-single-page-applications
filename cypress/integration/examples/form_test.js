54f2cae9cc27c9e4d55972a186cf796283cc9d80
KamrynR <62856359+KamrynR@users.noreply.github.com>
2020-07-20 18:03:19 -0500

    retake

--- cypress/integration/examples/form_test.js
+++ cypress/integration/examples/form_test.js
@@ -1,16 +1,18 @@
 describe('Test', function () {
     it('Tests for a name', function () {
-        cy.visit('http://localhost:3000/pizza')
+        cy.visit('http://localhost:3000/pizza');
 
-        cy.get(':nth-child(2) > input')
-            .should('have.value', '')
+        cy.get(':nth-child(2) > input');
+            .type('james')
+            .should('have.value', 'james');
 
         cy.get('.pepperoni > p > input')
-            .should('have.value', 'on')
+            .should('have.value', 'on');
 
         cy.get('.canadianBacon > p > input')
-        .should('have.value', 'on')
+            .should('have.value', 'on');
 
-        cy.contains('.order-button').click()
+        cy.get('.order-button')
+            .click()
     })
 })
